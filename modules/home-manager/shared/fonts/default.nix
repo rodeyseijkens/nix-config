ad5af723d9ef3b9f28e7fa77e8271e36439541d4
Rodey Seijkens <me@rodey.nl>
2025-01-03 23:40:36 +0100

    feat: :sparkles: added "NF caskaydia-cove" & "fantasque-sans-mono"

--- modules/home-manager/shared/fonts/default.nix
+++ modules/home-manager/shared/fonts/default.nix
@@ -3,9 +3,11 @@
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
+    nerd-fonts.caskaydia-cove
     nerd-fonts.symbols-only
     twemoji-color-font
     noto-fonts-emoji
+    fantasque-sans-mono
     maple-mono
   ];
 }
