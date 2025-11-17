05d1c8f3e1a7512fcba9d8acb337cbb64081944b
Rodey Seijkens <me@rodey.nl>
2025-11-17 18:01:19 +0100

    feat: :sparkles: update vscode

--- modules/home-manager/shared/vscode.nix
+++ modules/home-manager/shared/vscode.nix
@@ -8,115 +8,124 @@ with lib; let
   marketplace = inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace;
   marketplace-release = inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace-release;
 in {
-    programs.vscode = {
-      enable = true;
+  programs.vscode = {
+    enable = true;
 
-      mutableExtensionsDir = true;
+    mutableExtensionsDir = true;
 
-      profiles.default = {
-        extensions =
-          (with pkgs.vscode-extensions; [
-            # nix language
-            bbenoist.nix
+    profiles.default = {
+      extensions =
+        (with pkgs.vscode-extensions; [
+          # nix language
+          bbenoist.nix
 
-            # nix formatting
-            kamadorueda.alejandra
-          ])
-          ++ (with marketplace; [
-            # github.copilot
+          # nix formatting
+          kamadorueda.alejandra
+        ])
+        ++ (with marketplace; [
+          # github.copilot
 
-            vivaxy.vscode-conventional-commits # commit message helper
-            eamodio.gitlens
-            dbaeumer.vscode-eslint
-            esbenp.prettier-vscode
+          vivaxy.vscode-conventional-commits # commit message helper
+          eamodio.gitlens # git helper
+          dbaeumer.vscode-eslint # eslint
+          esbenp.prettier-vscode # prettier
+          editorconfig.editorconfig # editorconfig
+          tamasfe.even-better-toml # toml
+          yoavbls.pretty-ts-errors # pretty ts errors
+          wallabyjs.quokka-vscode # quokka dev tool
+          mechatroner.rainbow-csv # rainbow csv
+          bradlc.vscode-tailwindcss # tailwindcss
+          gruntfuggly.todo-tree # todo helper
+          pflannery.vscode-versionlens # version lens
+          vscodevim.vim # vim
 
-            # Color theme
-            jdinhlife.gruvbox
-            jonathanharty.gruvbox-material-icon-theme
-          ])
-          ++ (with marketplace-release; [
-            # github.copilot-chat
-          ]);
-        userSettings = {
-          "update.mode" = "none";
+          # Color theme
+          jdinhlife.gruvbox
+          jonathanharty.gruvbox-material-icon-theme
+        ])
+        ++ (with marketplace-release; [
+          # github.copilot-chat
+        ]);
+      userSettings = {
+        "update.mode" = "none";
 
-          "editor.fontFamily" = "'Maple Mono', 'SymbolsNerdFont', 'monospace', monospace";
-          "editor.fontLigatures" = true;
-          "editor.fontSize" = 16;
+        "editor.fontLigatures" = true;
 
-          "editor.formatOnPaste" = true;
-          "editor.formatOnSave" = true;
-          "editor.formatOnType" = true;
+        "editor.formatOnPaste" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnType" = false;
 
-          "editor.codeActionsOnSave" = {
-            "source.fixAll.eslint" = "explicit";
-          };
-
-          "editor.minimap.enabled" = true;
-          "editor.mouseWheelZoom" = true;
-          "editor.renderControlCharacters" = false;
-          "editor.scrollbar.horizontalScrollbarSize" = 5;
-          "editor.scrollbar.verticalScrollbarSize" = 5;
-
-          "explorer.confirmDragAndDrop" = false;
-          "explorer.openEditors.visible" = 0;
-
-          "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
-
-          "files.autoSave" = "onWindowChange";
-          "terminal.integrated.fontFamily" = "'Maple Mono', 'SymbolsNerdFont'";
-          "vsicons.dontShowNewVersionMessage" = true;
-
-          "window.customTitleBarVisibility" = "auto";
-          "window.menuBarVisibility" = "toggle";
-
-          "workbench.activityBar.location" = "top";
-          "workbench.colorTheme" = "Gruvbox Dark Hard";
-          "workbench.editor.limit.enabled" = true;
-          "workbench.editor.limit.perEditorGroup" = true;
-          "workbench.editor.limit.value" = 10;
-          "workbench.iconTheme" = "gruvbox-material-icon-theme";
-          "workbench.layoutControl.enabled" = false;
-          "workbench.layoutControl.type" = "menu";
-          "workbench.startupEditor" = "none";
-          "workbench.statusBar.visible" = true;
-          "workbench.panel.showLabels" = false;
-
-          # Extension settings
-          "alejandra.program" = "alejandra";
-          "material-icon-theme.folders.theme" = "classic";
-          "gitlens.rebaseEditor.ordering" = "asc";
-          "[nix]" = {
-            "editor.defaultFormatter" = "kamadorueda.alejandra";
-            "editor.formatOnPaste" = true;
-            "editor.formatOnSave" = true;
-            "editor.formatOnType" = false;
-          };
-          "[json]" = {
-            "editor.defaultFormatter" = "vscode.json-language-features";
-          };
-          "[typescript]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[javascript]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[typescriptreact]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
+        "editor.codeActionsOnSave" = {
+          "source.fixAll.eslint" = "explicit";
         };
-        # Keybindings
-        keybindings = [
-          {
-            key = "ctrl+s";
-            command = "workbench.action.files.saveFiles";
-          }
-          {
-            "key" = "ctrl+alt+meta+delete";
-            "command" = "workbench.action.terminal.killAll";
-            "when" = "terminalHasBeenCreated || terminalIsOpen || terminalProcessSupported";
-          }
-        ];
+
+        "editor.minimap.enabled" = true;
+        "editor.mouseWheelZoom" = true;
+        "editor.renderControlCharacters" = false;
+        "editor.scrollbar.horizontalScrollbarSize" = 5;
+        "editor.scrollbar.verticalScrollbarSize" = 5;
+
+        "explorer.confirmDragAndDrop" = false;
+        "explorer.openEditors.visible" = 0;
+
+        "extensions.autoUpdate" = false; # This stuff fixes vscode freaking out when theres an update
+
+        "files.autoSave" = "onWindowChange";
+        "terminal.integrated.fontFamily" = "'Maple Mono', 'SymbolsNerdFont'";
+        "vsicons.dontShowNewVersionMessage" = true;
+
+        "window.customTitleBarVisibility" = "auto";
+        "window.menuBarVisibility" = "toggle";
+
+        "workbench.activityBar.location" = "top";
+        "workbench.colorTheme" = mkForce "Gruvbox Dark Hard";
+        "workbench.editor.limit.enabled" = true;
+        "workbench.editor.limit.perEditorGroup" = true;
+        "workbench.editor.limit.value" = 10;
+        "workbench.iconTheme" = "gruvbox-material-icon-theme";
+        "workbench.layoutControl.enabled" = false;
+        "workbench.layoutControl.type" = "menu";
+        "workbench.startupEditor" = "none";
+        "workbench.statusBar.visible" = true;
+        "workbench.panel.showLabels" = false;
+        "workbench.panel.defaultLocation" = "right";
+
+        # Extension settings
+        "alejandra.program" = "alejandra";
+        "material-icon-theme.folders.theme" = "classic";
+        "gitlens.rebaseEditor.ordering" = "asc";
+        "github.copilot.nextEditSuggestions.enabled" = true;
+        "vim.disableExtension" = true;
+        "[nix]" = {
+          "editor.defaultFormatter" = "kamadorueda.alejandra";
+        };
+        "[json]" = {
+          "editor.defaultFormatter" = "vscode.json-language-features";
+        };
+        "[typescript]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "[javascript]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "[typescriptreact]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "[css]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
       };
+      # Keybindings
+      keybindings = [
+        {
+          "command" = "workbench.action.files.saveFiles";
+          "key" = "ctrl+s";
+        }
+        {
+          "command" = "workbench.action.terminal.killAll";
+          "key" = "ctrl+alt+meta+delete";
+        }
+      ];
     };
+  };
 }
